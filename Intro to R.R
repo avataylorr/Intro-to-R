536c6e64560227a4885a0f31d5b1316e12a54bcb
Ava Florczak <avf223@lehigh.edu>
2019-09-25 18:38:19 -0400

    i added pizza to 5 and 7

--- Intro to R.R
+++ Intro to R.R
@@ -1,9 +1,9 @@
-# R script to accopany Intro to R
-# Copy for RStudent 2019
+# R script to accompany Intro to R
+# Copy for Ava Florczak 2019
 ######################################################
 
-# You will see that you can put non-executable "comment" lines
+# You will see that you can put non-executable "comment" lines. pizza
 # in your script by putting "#" at the beginning of the lines.
-# 
+# pizza
 # In fact, you can put comments anywhere on a line as long as 
 # you put "#" before them.
